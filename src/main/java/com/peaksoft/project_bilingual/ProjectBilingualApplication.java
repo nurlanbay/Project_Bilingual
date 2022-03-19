33e72c33e7eed8534be76aa35c57b211c6a91adf
nurlanbey <nbnskg97@gmail.com>
2022-03-18 18:51:13 -1200

    Initial commit

---
+++ src/main/java/com/peaksoft/project_bilingual/ProjectBilingualApplication.java
@@ -0,0 +1,13 @@
+package com.peaksoft.project_bilingual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBilingualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBilingualApplication.class, args);
+        
+    }
+}
---
+++ src/test/java/com/peaksoft/project_bilingual/ProjectBilingualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peaksoft.project_bilingual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBilingualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
